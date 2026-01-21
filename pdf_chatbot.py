fdab43b3a084025df82d375df0f2ed4ffccdb0b5
RAGHAVA-tech <bhavishyab@outlook.com>
2026-01-21 14:55:06 +0530

    Update pdf_chatbot.py

--- pdf_chatbot.py
+++ pdf_chatbot.py
@@ -6,7 +6,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_classic.memory import ConversationBufferMemory
 from langchain_classic.chains import ConversationalRetrievalChain
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 import os
 
 # Set OpenAI API key from secrets
@@ -131,6 +131,7 @@ if st.session_state.processComplete:
 # Display initial instructions
 else:
     st.write("👈 Upload your PDFs in the sidebar to get started!")
+
 
 
 
